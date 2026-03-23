47d69c6beb34f6d3edde208b1b11efc844c1452e
jerryjerry2 <chmapikeat@gmail.com>
2026-03-23 15:44:53 +0700

    add code

--- A305/RealAPI/controllers/categoryController.js
+++ A305/RealAPI/controllers/categoryController.js
@@ -1,8 +1,9 @@
 const pool = require('../config/db');
+const categoryService = require('../services/categoryService');
 
 const getAll = async (req, res) => {
     try {
-        const [rows] = await pool.query('select * from category');
+        const rows = await categoryService.getAll();
 
         res.status(200).json({
             result : true,
@@ -18,6 +19,72 @@ const getAll = async (req, res) => {
     }
 }
 
+const create = async (req, res) => {
+    try {
+        let result = await categoryService.create(req.body);
+        
+        res.json({
+            result : true,
+            msg : 'Create Category Successfully',
+            data : result[0]
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({
+            result : false,
+            msg : 'Internal Server Error',
+        })
+    }
+}
+
+const update = async (req, res) => {
+    try {
+        const result = await categoryService.update(req.body, req.params.id);
+        
+        return  res.json({
+            result : true,
+            msg : 'Update Category Successfully',
+            data : result[0]
+        })
+
+    } catch (error) {
+        console.log(error);
+        return  res.json({
+            result : false,
+            msg : 'Internal Server Error',
+        })
+    }
+}
+
+const remove = async (req, res) => {
+    try {
+        let row = await categoryService.getById(req.params.id);
+     
+        if(row.length == 0){
+            return res.json({
+                result : false,
+                msg : 'Category not found'
+            })
+        }
+        
+        await categoryService.remove(req.params.id);
+
+        return res.json({
+            result : true,
+            msg : 'Delete Category successfully'
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            result : false,
+            msg : 'Internal Server Error'
+        })
+    }
+}
+
 module.exports = {
-    getAll
+    getAll,
+    create,
+    update,
+    remove,
 }
