5dc91f65394c797e807fa792f8e2c177ae91206d
jerryjerry2 <chmapikeat@gmail.com>
2026-03-23 15:16:32 +0700

    add code

--- A305/RealAPI/routes/categoryRoutes.js
+++ A305/RealAPI/routes/categoryRoutes.js
@@ -6,79 +6,11 @@ const categoryController = require('../controllers/categoryController');
 
 router.get('/category', categoryController.getAll);
 
-router.post('/category', async (req, res) => {
-    try {
-        let sql = 'insert into category (name) values (?)';
-        let body = req.body;
-        let data = [body.name];
-        let [result] = await pool.query(sql, data);
-        let [row] =  await pool.query('select * from category where id = ?', [result.insertId]);
-        console.log(row);
-        
-        res.json({
-            result : true,
-            msg : 'Create Category Successfully',
-            data : row[0]
-        })
-    } catch (error) {
-        console.log(error);
-        res.json({
-            result : false,
-            msg : 'Internal Server Error',
-        })
-    }
-});
+router.post('/category', categoryController.create);
 
-router.put('/category/:id', async (req, res) => {
-    try {
-        console.log(req.params.id);
-        console.log(req.body);
-        let sql = 'update category set name = ? where id = ?';
-        let body = req.body;
-        let data = [body.name, req.params.id];
-        let [result] = await pool.query(sql, data);
-        let [row] =  await pool.query('select * from category where id = ?', [req.params.id]);
-        console.log(row);
-        
-        return  res.json({
-            result : true,
-            msg : 'Update Category Successfully',
-            data : row[0]
-        })
+router.put('/category/:id', categoryController.update);
 
-    } catch (error) {
-        console.log(error);
-        return  res.json({
-            result : false,
-            msg : 'Internal Server Error',
-        })
-    }
-})
-
-router.delete('/category/:id', async (req, res) => {
-    try {
-        let [row] = await pool.query('select * from category where id = ?', [req.params.id]);
-     
-        if(row.length == 0){
-            return res.json({
-                result : false,
-                msg : 'Category not found'
-            })
-        }
-        let [result] = await pool.query('delete from category where id = ?', [req.params.id]);
-
-        return res.json({
-            result : true,
-            msg : 'Delete Category successfully'
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            result : false,
-            msg : 'Internal Server Error'
-        })
-    }
-})
+router.delete('/category/:id', categoryController.remove)
 
 
 module.exports = router;
