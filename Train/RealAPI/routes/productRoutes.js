be742d91e2514eab0078a2f4ec1c311d5f31d0de
jerryjerry2 <chmapikeat@gmail.com>
2026-03-25 00:06:48 +0700

    add file

--- Train/RealAPI/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const productController = require('../controller/productController');
-
-const router = express.Router();
-
-router.get('/products', productController.getAll);
-
-module.exports = router;
