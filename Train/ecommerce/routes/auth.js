c0c5e62791d64029c3d9068df7adc541d4724135
jerryjerry2 <chmapikeat@gmail.com>
2026-04-02 23:52:14 +0700

    add code

--- Train/ecommerce/routes/auth.js
+++ Train/ecommerce/routes/auth.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', isLogin, getMe);
+router.delete('/logout,');
 
 module.exports = router;
